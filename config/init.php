677dd36dd6ab38d5b296263857c92ddf09cf28bb
reven0ir <michail.yashchuck@gmail.com>
2025-05-15 21:35:55 +0300

    add initial configuration file

---
+++ config/init.php
@@ -0,0 +1,12 @@
+<?php
+
+define("ROOT", dirname(__DIR__));
+
+const WWW = ROOT . '/public';
+const APP = ROOT . '/app';
+const CORE = ROOT . '/core';
+const HELPERS = ROOT . '/helpers';
+const CONFIG = ROOT . '/config';
+const VIEWS = APP . '/Views';
+const LAYOUT = 'default';
+const PATH = 'http://localhost';
