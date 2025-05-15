af8ca1d18336a2e54dda7774871cf47dff756845
reven0ir <michail.yashchuck@gmail.com>
2025-05-15 21:38:02 +0300

    add contact form page

---
+++ app/Views/contact.php
@@ -0,0 +1,28 @@
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-6 offset-md-3">
+
+            <h1>Contact form page</h1>
+
+            <form action="" method="post">
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" id="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+
+                <div class="mb-3">
+                    <label for="content" class="form-label">Content</label>
+                    <textarea name="content" id="content" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Send</button>
+
+            </form>
+
+        </div>
+
+    </div>
+</div>
