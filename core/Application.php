727f4e5d7f85dcf28618c11398097e997c2fd875
reven0ir <michail.yashchuck@gmail.com>
2025-05-15 21:37:15 +0300

    add core Application class

---
+++ core/Application.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MVCore;
+
+class Application
+{
+    public string $uri;
+    public Request $request;
+    public Response $response;
+    public Router $router;
+    public View $view;
+    public static Application $app;
+
+
+    public function __construct()
+    {
+        self::$app = $this;
+        $this->uri = $_SERVER['QUERY_STRING'];
+        $this->request = new Request($this->uri);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+        $this->view = new View(LAYOUT);
+    }
+
+    public function run(): void
+    {
+        echo $this->router->dispatch();
+    }
+}
