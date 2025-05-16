d534b67ee412f67e52b1bf5ab5c70b936b583ad9
reven0ir <michail.yashchuck@gmail.com>
2025-05-16 21:57:34 +0300

    add redirect method to Response class

--- core/Response.php
+++ core/Response.php
@@ -8,4 +8,17 @@ class Response
     {
         http_response_code($code);
     }
+
+    //TODO: add response code 302
+    public function redirect(string $url = '', int $code = 302): never
+    {
+        if ($url) {
+            $redirect_url = $url;
+        } else {
+            $redirect_url = $_SERVER['HTTP_REFERER'] ?? base_url();
+        }
+
+        header("Location: {$redirect_url}", true, $code);
+        die;
+    }
 }
