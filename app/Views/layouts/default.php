a15e74a7b81c8ca239dc18164f3268fb30dc7fe7
reven0ir <michail.yashchuck@gmail.com>
2025-05-16 21:56:37 +0300

    refactor asset URL to use base_url() instead of PATH.

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -6,8 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MVCore :: <?= $title ?? ''; ?></title>
-    <link rel="stylesheet" href="<?= PATH; ?>/assets/bootstrap/css/bootstrap.css">
-    <link rel="icon" href="<?= PATH; ?>/assets/images/framework.png">
+    <link rel="stylesheet" href="<?= base_url('/'); ?>assets/bootstrap/css/bootstrap.css">
+    <link rel="icon" href="<?= base_url('/'); ?>assets/images/framework.png">
 <!--
     <a href="https://www.flaticon.com/free-icons/framework" title="framework icons">Framework icons created by SBTS2018 - Flaticon
 </a>-->
@@ -35,6 +35,6 @@
 
 <?= $this->content; ?>
 
-<script src="<?= PATH; ?>/assets/bootstrap/js/bootstrap.js"></script>
+<script src="<?= base_url('/'); ?>assets/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
