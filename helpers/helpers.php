5b57c10c9c29a8e4a848e73028a7e177668abd9d
reven0ir <michail.yashchuck@gmail.com>
2025-05-16 21:56:47 +0300

    add helper functions for response, validation, and error handling

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -20,7 +20,54 @@ function request(): \MVCore\Request
     return app()->request;
 }
 
+function response(): \MVCore\Response
+{
+    return app()->response;
+}
+
 function base_url($path = ''): string
 {
     return PATH . $path;
+}
+
+function h($string): string
+{
+    return htmlspecialchars($string, ENT_QUOTES);
+}
+
+function old($field_name): string
+{
+    return isset($_POST[$field_name]) ? h($_POST[$field_name]) : '';
+}
+
+function get_errors($field_name, $errors = []): string
+{
+    $output = '';
+
+    if (isset($errors[$field_name])) {
+        $output .= '<div class="invalid-feedback d-block"><ul class="list-unstyled"></ul>';
+        foreach ($errors[$field_name] as $error) {
+            $output .= "<li>{$error}</li>";
+        }
+        $output .= '</ul></div>';
+    }
+
+    return $output;
+}
+
+function get_validation_class($field_name, $errors = []): string
+{
+    if (empty($errors)) {
+        return '';
+    } else {
+        return isset($errors[$field_name]) ? 'is-invalid' : 'is-valid';
+    }
+}
+
+function abort($error = '', $code = 404)
+{
+    response()->setResponseCode($code);
+    echo view("errors/{$code}", ['error' => $error], false);
+
+    die;
 }
