3fd12e4244ef9e5c2434c616dc6128498882c5b7
reven0ir <michail.yashchuck@gmail.com>
2025-05-17 20:11:48 +0300

    reorganize helper inclusion and remove debug code

--- public/index.php
+++ public/index.php
@@ -8,13 +8,9 @@ if (PHP_MAJOR_VERSION < 8) {
 
 require_once __DIR__ . '/../config/init.php';
 require_once ROOT . '/vendor/autoload.php';
+require_once HELPERS . '/helpers.php';
 
 $app = new \MVCore\Application();
 require_once CONFIG . '/routes.php';
-require_once HELPERS . '/helpers.php';
-
-//dump($app->router->getRoutes());
-//dump($app->request->get('page'));
-//dump($app->request->get('s2', 'abs'));
 
 $app->run();
