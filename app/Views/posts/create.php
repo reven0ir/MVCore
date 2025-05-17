b05ecc1073bd327213f36403bd43524febd237a1
reven0ir <michail.yashchuck@gmail.com>
2025-05-18 00:38:05 +0300

    add view for creating posts

---
+++ app/Views/posts/create.php
@@ -0,0 +1,30 @@
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-6 offset-md-3">
+
+            <h1>Create post</h1>
+
+            <form action="<?= base_url('/posts/store') ?>" method="post">
+
+                <div class="mb-3">
+                    <label for="title" class="form-label">Title</label>
+                    <input type="text" name="title" id="title" class="form-control <?= get_validation_class('title', $errors ?? []) ?>" placeholder="Title" value="<?= old('title') ?>">
+                    <?= get_errors('title', $errors ?? []) ?>
+                </div>
+
+                <div class="mb-3">
+                    <label for="content" class="form-label">Content</label>
+                    <textarea name="content" id="content" class="form-control <?= get_validation_class('content', $errors ?? []) ?>" rows="3"><?= old('content') ?></textarea>
+                    <?= get_errors('content', $errors ?? []) ?>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Send</button>
+
+            </form>
+
+        </div>
+
+    </div>
+</div>
