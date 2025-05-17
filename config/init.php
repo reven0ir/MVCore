22fa02a517d72b37a7d649826f0b4c40b9e87573
reven0ir <michail.yashchuck@gmail.com>
2025-05-17 20:12:00 +0300

    add debug mode and database configuration constants

--- config/init.php
+++ config/init.php
@@ -2,6 +2,7 @@
 
 define("ROOT", dirname(__DIR__));
 
+const DEBUG = true;
 const WWW = ROOT . '/public';
 const APP = ROOT . '/app';
 const CORE = ROOT . '/core';
@@ -10,3 +11,14 @@ const CONFIG = ROOT . '/config';
 const VIEWS = APP . '/Views';
 const LAYOUT = 'default';
 const PATH = 'http://localhost';
+const DB = [
+    'host'      =>  'localhost',
+    'user'      =>  'revenoir',
+    'password'  =>  '',
+    'dbname'  =>  'mvcore',
+    'charset'   =>  'utf8',
+    'options'   =>  [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ],
+];
