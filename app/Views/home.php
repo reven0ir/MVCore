7afad261c3e24afa0f5d5a525320f3482d35fa63
reven0ir <michail.yashchuck@gmail.com>
2025-05-17 20:11:36 +0300

    add dynamic post listing to the home page

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,3 +1,9 @@
 <div class="container">
     <h1>Home page</h1>
+
+    <?php if (!empty($posts)): ?>
+        <?php foreach ($posts as $post): ?>
+            <h3><?= $post['title']; ?></h3>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
