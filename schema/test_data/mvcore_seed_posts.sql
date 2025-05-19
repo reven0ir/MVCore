a4fcb5534250ce5b4bf80cfc1fc52cb6cbd2e355
reven0ir <michail.yashchuck@gmail.com>
2025-05-19 18:38:21 +0300

    add test seed data for posts in mvcore_seed_posts.sql
    this commit introduces initial seed data for the `posts` table. the data includes three entries with titles and descriptions for testing.

---
+++ schema/test_data/mvcore_seed_posts.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts (title, content) VALUES ('First post', 'First description');
+INSERT INTO posts (title, content) VALUES ('Second post', 'Second description');
+INSERT INTO posts (title, content) VALUES ('Thirst post', 'Thirst description');
