b43ee7a50a2aead5066da46fe5e7b766b29548a5
reven0ir <michail.yashchuck@gmail.com>
2025-05-24 19:46:25 +0300

    add reusable alert partials for error, info, and success

---
+++ app/Views/partials/alert_info.php
@@ -0,0 +1,6 @@
+<div class="container my-2">
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <?php echo $flash_info ?? ''; ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</div>
