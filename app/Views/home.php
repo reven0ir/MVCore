af7f9eed0e2a586aaa44482ccd43bf8fc0f27f9c
reven0ir <michail.yashchuck@gmail.com>
2025-05-24 19:48:59 +0300

    add edit and delete links for posts in home view

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,9 +1,12 @@
 <div class="container">
     <h1>Home page</h1>
 
+    <?php //echo session()->get('name', 'Unknown'); ?>
+
     <?php if (!empty($posts)): ?>
         <?php foreach ($posts as $post): ?>
-            <h3><?= $post['title']; ?></h3>
+            <h3><a href="#"><?= $post['title']; ?></a> | <a href="<?= base_url('/posts/edit?id=' . $post['id']) ?>">EDIT</a> | <a href="<?= base_url('/posts/delete?id=' . $post['id']) ?>">DELETE</a></h3>
         <?php endforeach; ?>
     <?php endif; ?>
+
 </div>
