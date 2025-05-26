b631f353a68f7c0b35cece34d32bf2ca5bb1f2bd
reven0ir <michail.yashchuck@gmail.com>
2025-05-26 21:24:48 +0300

    add gallery support to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,8 +22,13 @@ class Post extends Model
             'unique' => 'posts,slug',
         ],
         'thumbnail' => [
-            'required' => true,
+            'file' => true,
             'extension' => 'jpg,jpeg,png',
+            'size' => 1_048_576, // 1MB
+        ],
+        'gallery' => [
+            'extension' => 'jpg,jpeg,png',
+            'size' => 1_048_576, // 1MB
         ]
     ];
     public array $labels = [
@@ -31,6 +36,35 @@ class Post extends Model
         'content' => 'Post content',
         'slug' => 'Slug',
         'thumbnail' => 'Thumbnail',
+        'gallery' => 'Gallery',
     ];
+
+    public function savePost(): false|string
+    {
+        $thumbnail = $this->attributes['thumbnail']['name'] ? $this->attributes['thumbnail'] : null;
+        unset($this->attributes['thumbnail']);
+
+        $gallery = $this->attributes['gallery']['name'][0] ? $this->attributes['gallery'] : null;
+        unset($this->attributes['gallery']);
+
+        $id = $this->save();
+
+        if ($thumbnail) {
+            if ($file_url = upload_file($thumbnail)) {
+                db()->query("UPDATE posts SET thumbnail = ? WHERE id = ?", [$file_url, $id]);
+            }
+        }
+
+        // TODO: optimize the query
+        if ($gallery) {
+            for ($i = 0; $i < count($gallery['name']); $i++) {
+                if ($file_url = upload_file($gallery, $i)) {
+                    db()->query("INSERT INTO gallery (post_id, image) VALUES (?, ?)", [$id, $file_url]);
+                }
+            }
+        }
+
+        return $id;
+    }
 
 }
