e59b5ec2c5f2aafc66293c2202e308a6de2fbdf4
reven0ir <michail.yashchuck@gmail.com>
2025-05-26 21:25:26 +0300

    add UPLOADS constant to init.php

--- config/init.php
+++ config/init.php
@@ -5,6 +5,7 @@ define("ROOT", dirname(__DIR__));
 const DEBUG = true;
 const ERROR_LOG_FILE = ROOT . '/tmp/error.log';
 const WWW = ROOT . '/public';
+const UPLOADS = WWW . '/uploads';
 const APP = ROOT . '/app';
 const CORE = ROOT . '/core';
 const HELPERS = ROOT . '/helpers';
