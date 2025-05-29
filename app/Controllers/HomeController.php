a9a17b58603befccf6a5629b050eeef6b929d516
reven0ir <michail.yashchuck@gmail.com>
2025-05-29 05:29:44 +0300

    added pagination in HomeController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controllers;
 
+use MVCore\Pagination;
+
 class HomeController extends BaseController
 {
 
     public function index()
     {
-        $posts = db()->findAll('posts');
-        return view('home', ['title' => 'Home', 'posts' => $posts]);
+        $page = request()->get('page', 1);
+        $total = db()->count('posts');
+        $per_page = 3;
+        $pagination = new Pagination((int)$page, $per_page, $total);
+        $start = $pagination->getStart();
+        $posts = db()->query("SELECT * FROM posts ORDER BY id DESC LIMIT :limit OFFSET :offset", ['limit' => $per_page, 'offset' => $start])->get();
+
+        return view('home', ['title' => 'Home', 'posts' => $posts, 'pagination' => $pagination]);
     }
 }
