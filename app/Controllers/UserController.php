8d7cb1733538ae936b66042bf14a6cb5751ac47f
Mykhailo Yashchuk <michail.yashchuck@gmail.com>
2025-05-30 20:43:49 +0300

    add user authentication (register, login, logout)

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -2,16 +2,64 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+use MVCore\View;
+
 class UserController extends BaseController
 {
 
-    public function register()
+    public function register(): View|string
     {
         if (request()->isPost()) {
-            return 'Register POST data';
+            $model = new User();
+            $model->loadData();
+            if (!$model->validate()) {
+                return view('users/register', ['title' => 'Sign Up', 'errors' => $model->getError()]);
+            }
+
+            $model->attributes['password'] = password_hash($model->attributes['password'], PASSWORD_DEFAULT);
+            unset($model->attributes['repassword']);
+            if ($model->save()) {
+                session()->setFlash('success', 'Successfully registered.');
+                response()->redirect('/login');
+            } else {
+                session()->setFlash('error', 'Error registering.');
+            }
         }
 
-        return 'Show register form';
+        return view('users/register', ['title' => 'Sign Up']);
+    }
+
+    public function login(): View|string
+    {
+        if (request()->isPost()) {
+            $model = new User();
+            $model->loadData();
+            if (!$model->validate($model->attributes, [
+                'email' => ['required' => true],
+                'password' => ['required' => true],
+            ])) {
+                return view('users/login', ['title' => 'Sign In', 'errors' => $model->getError()]);
+            }
+
+            if ($model->auth()) {
+                session()->setFlash('success', 'Successfully logged.');
+                response()->redirect('/');
+            } else {
+                session()->setFlash('error', 'Incorrect email or password.');
+                response()->redirect('/login');
+            }
+        }
+
+        return view('users/login', ['title' => 'Sign In']);
+    }
+
+    public function logout(): void
+    {
+        if (check_auth()) {
+            session()->delete('user');
+        }
+        response()->redirect('/');
     }
 
 }
