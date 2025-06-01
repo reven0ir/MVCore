62976caaee7ed44f2fa669bdf5f605dd5aa2ed2b
Mykhailo Yashchuk <michail.yashchuck@gmail.com>
2025-06-01 19:27:49 +0300

    add file upload and email sending to contact a form

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -19,11 +19,41 @@ class ContactController extends BaseController
     {
         $model = new Contact();
         $model->loadData();
+
+        if (isset($_FILES['thumbnail'])) {
+            $model->attributes['thumbnail'] = $_FILES['thumbnail'];
+        } else {
+            $model->attributes['thumbnail'] = [];
+        }
+
         if(!$model->validate()) {
             return view('contact', ['title' => 'Contact form', 'errors' => $model->getError()]);
         }
 
-        response()->redirect('/');
+        $files = [];
+        if ($model->attributes['thumbnail']['error'] === 0) {
+            $files[] = upload_file($model->attributes['thumbnail'], path: true);
+        }
+
+        $content = nl2br($model->attributes['content']);
+        $body = "
+            <b>Name:</b> {$model->attributes['name']}<br>
+            <b>Email:</b> {$model->attributes['email']}<br>
+            <b>Content:</b> {$content}<br>
+        ";
+
+        if (send_mail([EMAIL['from_email']], 'Mail from MVCore', $body, $files)) {
+            session()->setFlash('success', 'Mail sent');
+            if ($files) {
+                foreach ($files as $file) {
+                    unlink($file);
+                }
+            }
+        } else{
+            session()->setFlash('error', 'Error sending mail');
+        }
+
+        response()->redirect('/contact');
     }
 
 }
