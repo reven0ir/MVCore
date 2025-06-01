202ade6b58c81b3a917a74a2b37e6581b3f754e6
Mykhailo Yashchuk <michail.yashchuck@gmail.com>
2025-06-01 17:30:01 +0300

    add middleware to routes

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,12 @@
 <?php
-/** @var \MVCore\Application $app **/
+/** @var MVCore\Application $app **/
+
+use MVCore\Middleware\Auth;
+
+const MIDDLEWARE = [
+    'auth' => \MVCore\Middleware\Auth::class,
+    'guest' => \MVCore\Middleware\Guest::class,
+];
 
 $app->router->get('/', [\App\Controllers\HomeController::class, 'index']);
 
@@ -11,18 +18,18 @@ $app->router->get('/about', function () {
 $app->router->get('/contact', [\App\Controllers\ContactController::class, 'index']);
 $app->router->post('/contact', [\App\Controllers\ContactController::class, 'send']);
 
-$app->router->get('/posts/create', [\App\Controllers\PostController::class, 'create']);
-$app->router->post('/posts/store', [\App\Controllers\PostController::class, 'store']);
+$app->router->get('/posts/create', [\App\Controllers\PostController::class, 'create'])->only('auth');
+$app->router->post('/posts/store', [\App\Controllers\PostController::class, 'store'])->only('auth');
 
-$app->router->get('/posts/edit', [\App\Controllers\PostController::class, 'edit']);
-$app->router->post('/posts/update', [\App\Controllers\PostController::class, 'update']);
+$app->router->get('/posts/edit', [\App\Controllers\PostController::class, 'edit'])->only('auth');
+$app->router->post('/posts/update', [\App\Controllers\PostController::class, 'update'])->only('auth');
 
-$app->router->get('/posts/delete', [\App\Controllers\PostController::class, 'delete']);
-$app->router->post('/posts/update', [\App\Controllers\PostController::class, 'update']);
+$app->router->get('/posts/delete', [\App\Controllers\PostController::class, 'delete'])->only('auth');
+$app->router->post('/posts/update', [\App\Controllers\PostController::class, 'update'])->only('auth');
 
 $app->router->get('/posts/(?P<slug>[a-z0-9-]+)', [\App\Controllers\PostController::class, 'show']);
 
 // User
-$app->router->add('/register', [\App\Controllers\UserController::class, 'register'], ['get', 'post']);
-$app->router->add('/login', [\App\Controllers\UserController::class, 'login'], ['get', 'post']);
-$app->router->get('/logout', [\App\Controllers\UserController::class, 'logout']);
+$app->router->add('/register', [\App\Controllers\UserController::class, 'register'], ['get', 'post'])->only('guest');
+$app->router->add('/login', [\App\Controllers\UserController::class, 'login'], ['get', 'post'])->only('guest');
+$app->router->get('/logout', [\App\Controllers\UserController::class, 'logout'])->only('auth');
