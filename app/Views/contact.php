26d6e2a7dbcac7042030e6d625ba573b2e20f525
Mykhailo Yashchuk <michail.yashchuck@gmail.com>
2025-06-01 19:27:15 +0300

    remove a username field from content.php, add a thumbnail field to content.php

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -6,18 +6,12 @@
 
             <h1>Contact form page</h1>
 
-            <form action="" method="post">
+            <form action="" method="post" enctype="multipart/form-data">
 
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" name="name" id="name" class="form-control <?= get_validation_class('name', $errors ?? []) ?>" placeholder="John Johnson" value="<?= old('name') ?>">
                     <?= get_errors('name', $errors ?? []) ?>
-                </div>
-
-                <div class="mb-3">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" name="username" id="username" class="form-control <?= get_validation_class('username', $errors ?? []) ?>" placeholder="john_johnson" value="<?= old('username') ?>">
-                    <?= get_errors('username', $errors ?? []) ?>
                 </div>
 
                 <div class="mb-3">
@@ -30,6 +24,12 @@
                     <label for="content" class="form-label">Content</label>
                     <textarea name="content" id="content" class="form-control <?= get_validation_class('content', $errors ?? []) ?>" rows="3"><?= old('content') ?></textarea>
                     <?= get_errors('content', $errors ?? []) ?>
+                </div>
+
+                <div class="mb-3">
+                    <label for="thumbnail" class="form-label">Thumbnail</label>
+                    <input type="file" name="thumbnail" id="thumbnail" class="form-control <?= get_validation_class('thumbnail', $errors ?? []) ?>">
+                    <?= get_errors('thumbnail', $errors ?? []) ?>
                 </div>
 
                 <button type="submit" class="btn btn-primary">Send</button>
